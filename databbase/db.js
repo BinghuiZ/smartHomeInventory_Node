16b77e4cda23eea41d4d9ab7fad76021a29def20
zhongbinghuiGmail <zhongbinghui1995@gmail.com>
2019-04-19 18:03:09 +0800

    ben@20190418
    change to UATDB

--- databbase/db.js
+++ databbase/db.js
@@ -1,9 +1,26 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize("fyp", "root", "root", {
-    host: 'localhost',
+// const sequelize = new Sequelize("fyp", "root", "root", {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: '8889',
+//     operatorsAliases: false,
+//     timezone: '+08:00',
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+    
+//     storage: 'path/to/database.sqlite'
+// })
+
+const sequelize = new Sequelize("heroku_af94489beda5846", "b0cf461c9b8d87", "46f23291", {
+    host: 'us-cdbr-iron-east-02.cleardb.net',
     dialect: 'mysql',
-    port: '8889',
+    port: '3306',
     operatorsAliases: false,
     timezone: '+08:00',
 
