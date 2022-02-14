b03a9ecf2c0d8fc25cdf3f19e835b225d4daa30f
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2022-02-15 00:09:13 +0800

    save changes

--- databbase/db.js
+++ databbase/db.js
@@ -1,26 +1,9 @@
 const Sequelize = require('sequelize')
 const db = {}
-// const sequelize = new Sequelize("fyp", "root", "root", {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: '8889',
-//     operatorsAliases: false,
-//     timezone: '+08:00',
-
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     },
-    
-//     storage: 'path/to/database.sqlite'
-// })
-
-const sequelize = new Sequelize("heroku_af94489beda5846", "b0cf461c9b8d87", "46f23291", {
-    host: 'us-cdbr-iron-east-02.cleardb.net',
+const sequelize = new Sequelize("fyp", "root", "root", {
+    host: 'localhost',
     dialect: 'mysql',
-    port: '3306',
+    port: '8889',
     operatorsAliases: false,
     timezone: '+08:00',
 
@@ -33,6 +16,23 @@ const sequelize = new Sequelize("heroku_af94489beda5846", "b0cf461c9b8d87", "46f
     
     storage: 'path/to/database.sqlite'
 })
+
+// const sequelize = new Sequelize("heroku_af94489beda5846", "b0cf461c9b8d87", "46f23291", {
+//     host: 'us-cdbr-iron-east-02.cleardb.net',
+//     dialect: 'mysql',
+//     port: '3306',
+//     operatorsAliases: false,
+//     timezone: '+08:00',
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+    
+//     storage: 'path/to/database.sqlite'
+// })
 
 sequelize
     .authenticate()
