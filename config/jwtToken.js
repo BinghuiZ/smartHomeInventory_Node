9925dc3493365c3809219543871758f325fadecd
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2022-02-15 23:37:45 +0800

    - reinit project with Nestjs

--- config/jwtToken.js
+++
@@ -1,7 +0,0 @@
-let tokenConfig = {
-    secret:'Ben1234asdflkjhqwedfbhkvgyuhFDT^gyti^7yugcdriu%ER^Tu45e^RTI7yu5w^RITyueurtyIOUhgftjrtyIUjoluiJP"i0IJOP90u[i]gIRtyRItoyugFUErtgDJgDUEW42guhjb:Y*yt685&(%^O*790678o97ytRI%R6tTIR%57yTRU%^&*YTIuihgyO^7y89978o',
-    options:{
-        expiresIn: '1h',
-    }
-}
-module.exports = tokenConfig;
